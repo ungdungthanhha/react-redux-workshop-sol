e718a2e3825c98a86c2732fdd00d841478250876
ungdungthanhha <ungdungthanhha@gmail.com>
2026-06-01 23:12:37 +0700

    Done 06-useReducer

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import { CATEGORIES } from '../constants'
 import type { Expense } from '../types/expense'
 
@@ -6,25 +6,48 @@ interface ExpenseFormProps {
   onAddExpense: (expense: Omit<Expense, 'id'>) => void
 }
 
+type FormState = {
+  description: string,
+  amount: string,
+  category: string,
+}
+
+type FormAction = 
+  | {type: 'RESET'}
+  | {type: 'SET_FIELD'; field: string; value: string}
+
+const initialFormState: FormState = {
+  description: '',
+  amount: '',
+  category: '',
+}
+
+function formReducer(state: FormState, action: FormAction): FormState {
+  switch(action.type) {
+    case 'RESET':
+      return initialFormState;
+    case 'SET_FIELD':
+      return {...state, [action.field]: action.value};
+    default:
+      return state;
+  }
+}
+
 function ExpenseForm({ onAddExpense }: ExpenseFormProps) {
   // TODO: replace with useReducer(formReducer, initialFormState)
-  const [description, setDescription] = useState('')
-  const [amount, setAmount] = useState('')
-  const [category, setCategory] = useState('')
+  const [state, dispatch] = useReducer(formReducer, initialFormState)
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
-    if (!description || !amount || !category) return
+    if (!state.description || !state.amount || !state.category) return
     onAddExpense({
-      description,
-      amount: parseFloat(amount),
-      category,
+      description: state.description,
+      amount: parseFloat(state.amount),
+      category: state.category,
       date: new Date().toISOString().split('T')[0],
     })
     // TODO: dispatch({ type: 'RESET' })
-    setDescription('')
-    setAmount('')
-    setCategory('')
+    dispatch({ type: 'RESET' });
   }
 
   return (
@@ -33,9 +56,9 @@ function ExpenseForm({ onAddExpense }: ExpenseFormProps) {
       <label>
         Name
         <input
-          value={description}
+          value={state.description}
           // TODO: onChange => dispatch({ type: 'SET_FIELD', field: 'description', value: e.target.value })
-          onChange={e => setDescription(e.target.value)}
+          onChange={e => dispatch({ type:'SET_FIELD', field: 'description', value: e.target.value })}
           placeholder="e.g. Lunch"
           required
         />
@@ -43,9 +66,9 @@ function ExpenseForm({ onAddExpense }: ExpenseFormProps) {
       <label>
         Amount ($)
         <input
-          value={amount}
+          value={state.amount}
           // TODO: onChange => dispatch({ type: 'SET_FIELD', field: 'amount', value: e.target.value })
-          onChange={e => setAmount(e.target.value)}
+          onChange={e => dispatch({ type: 'SET_FIELD', field: 'amount', value: e.target.value })}
           type="number"
           placeholder="0.00"
           min={0}
@@ -55,7 +78,7 @@ function ExpenseForm({ onAddExpense }: ExpenseFormProps) {
       </label>
       <label>
         Category
-        <select value={category} onChange={e => setCategory(e.target.value)} required>
+        <select value={state.category} onChange={e => dispatch({ type: 'SET_FIELD', field: 'category', value: e.target.value })} required>
           {/* TODO: onChange => dispatch({ type: 'SET_FIELD', field: 'category', value: e.target.value }) */}
           <option value="">Select category</option>
           {CATEGORIES.map(c => (
