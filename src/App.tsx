0288e6dcb8b5b5766d1223d4edb5efec31a4e6cb
ungdungthanhha <ungdungthanhha@gmail.com>
2026-06-01 17:48:25 +0700

    Done 02-useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { Expense } from './types/expense'
 import ExpenseForm from './components/ExpenseForm'
 import ExpenseList from './components/ExpenseList'
 import './App.css'
 
 function App() {
-  const [expenses, setExpenses] = useState<Expense[]>([])
+  // Lazy initialization:
+  // read from localStorage during the initial state setup
+  // to prevent useEffect form accidentally
+  // overwriting saved data with an empty array
+  const [expenses, setExpenses] = useState<Expense[]>(() => {
+    try {
+      const savedData = localStorage.getItem('wecamp-expenses')
+      return savedData ? JSON.parse(savedData) : []
+    } catch (e) {
+      console.log("Error when loading data from local storage: ", e)
+      return []
+    }
+  })
 
   // TODO: load expenses from localStorage on mount
+  useEffect(() => {
+    const savedData = localStorage.getItem('wecamp-expenses')
+    if (savedData) 
+      setExpenses(JSON.parse(savedData))
+  }, [])
+
   // TODO: save expenses to localStorage whenever the list changes
+  useEffect(() => {
+    localStorage.setItem('wecamp-expenses', JSON.stringify(expenses))
+  }, [expenses])
 
   function handleAddExpense(expense: Omit<Expense, 'id'>) {
     setExpenses(prev => [
